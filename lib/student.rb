56763e1ba3bb57921f1bbc2f22928de0ee55914e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-19 18:18:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,17 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio]
+    @profile_url = student_hash[:profile_url]
+    
+    @@all << self
     
   end
 
@@ -17,7 +28,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
