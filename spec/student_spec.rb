7e10f977c54b1530fda9696591b7b2e467869f22
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-19 18:18:12 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -35,24 +35,7 @@ describe "Student" do
     end
   end
 
-  describe ".create_from_collection" do 
-    it "uses the Scraper class to create new students with the correct name and location." do 
-      Student.class_variable_set(:@@all, [])
-      Student.create_from_collection(student_index_array)
-      expect(Student.class_variable_get(:@@all).first.name).to eq("Alex Patriquin")
-    end
-  end
-
-  describe "#add_student_attributes" do 
-    it "uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student." do 
-      student.add_student_attributes(student_hash) 
-      expect(student.bio).to eq("I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school.")
-      expect(student.blog).to eq("someone@blog.com")
-      expect(student.linkedin).to eq("someone@linkedin.com")
-      expect(student.profile_quote).to eq("\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi")
-      expect(student.twitter).to eq("someone@twitter.com")
-    end
-  end
+  
 
   describe '.all' do
     it 'returns the class variable @@all' do
